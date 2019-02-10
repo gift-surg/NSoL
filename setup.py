27537aead029b1c2dfefb23327a3a5d5b9285d17
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2019-02-10 12:40:23 +0000

    ENH: use __about__.py for package meta data information

--- setup.py
+++ setup.py
@@ -9,10 +9,18 @@
 # \date       July 2017
 #
 
+import os
+
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+about = {}
+base_dir = os.path.dirname(__file__)
+with open(os.path.join(base_dir, "nsol", "__about__.py")) as fp:
+    exec(fp.read(), about)
 
 
 def install_requires(fname="requirements.txt"):
@@ -21,44 +29,43 @@ def install_requires(fname="requirements.txt"):
     content = [x.strip() for x in content]
     return content
 
-setup(name='NSoL',
-      version='0.2rc1',
-      description="The Numerical Solver Library (NSoL) is a research-focused "
-      "toolkit for denoising and deconvolution applications using ADMM and "
-      "Primal-Dual methods.",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/gift-surg/NSoL',
-      author='Michael Ebner',
-      author_email='michael.ebner.14@ucl.ac.uk',
-      license='BSD-3-Clause',
-      packages=find_packages(),
-      install_requires=install_requires(),
-      zip_safe=False,
-      keywords='development numericalsolver convexoptimisation',
-      classifiers=[
-          'Intended Audience :: Developers',
-          'Intended Audience :: Healthcare Industry',
-          'Intended Audience :: Science/Research',
+setup(
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    url=about["__uri__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
+    license=about["__license__"],
+    packages=find_packages(),
+    install_requires=install_requires(),
+    zip_safe=False,
+    keywords='development numericalsolver convexoptimisation',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Science/Research',
 
-          'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: BSD License',
 
-          'Topic :: Software Development :: Build Tools',
-          'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
 
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-      ],
-      entry_points={
-          'console_scripts': [
-              'nsol_corrupt_data = nsol.application.corrupt_data:main',
-              'nsol_run_deconvolution = nsol.application.run_deconvolution:main',
-              'nsol_run_deconvolution_study = nsol.application.run_deconvolution_study:main',
-              'nsol_run_denoising = nsol.application.run_denoising:main',
-              'nsol_run_denoising_study = nsol.application.run_denoising_study:main',
-              'nsol_show_parameter_study = nsol.application.show_parameter_study:main',
-          ],
-      },
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'nsol_corrupt_data = nsol.application.corrupt_data:main',
+            'nsol_run_deconvolution = nsol.application.run_deconvolution:main',
+            'nsol_run_deconvolution_study = nsol.application.run_deconvolution_study:main',
+            'nsol_run_denoising = nsol.application.run_denoising:main',
+            'nsol_run_denoising_study = nsol.application.run_denoising_study:main',
+            'nsol_show_parameter_study = nsol.application.show_parameter_study:main',
+        ],
+    },
 
-      )
+)
